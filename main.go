56869fc236b93e4ad36f90fd7f9ee6320976e8a3
marcetin <marcetin@gmail.com>
2020-07-05 03:22:25 +0200

    Init2

--- main.go
+++ main.go
@@ -7,13 +7,11 @@ import (
 	"gioui.org/widget"
 	"github.com/w-ingsolutions/c/pkg/gelook"
 	"github.com/w-ingsolutions/cgui/app"
-	in "github.com/w-ingsolutions/cgui/cfg/ini"
 	"log"
 	"os"
 )
 
 func main() {
-	in.Init()
 
 	w := calc.NewWingCal()
 	w.LinkoviIzboraVrsteRadova = map[int]*widget.Clickable{}
